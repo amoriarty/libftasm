83f914f26dae75c2df80d3ac02dddccc907c2f03
alegent <alegent@student.42.fr>
2015-07-04 12:45:10 +0200

    ft_strlen

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -5,22 +5,25 @@
 #                                                     +:+ +:+         +:+      #
 #    By: alegent <alegent@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2015/07/04 11:52:44 by alegent           #+#    #+#              #
-#    Updated: 2015/07/04 11:54:02 by alegent          ###   ########.fr        #
+#    Created: 2015/07/04 12:42:51 by alegent           #+#    #+#              #
+#    Updated: 2015/07/04 12:44:13 by alegent          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 global _ft_strlen
 
 _ft_strlen:
-	mov rax, 0
+	mov	rax, rdi
+	mov cl, [rax]
+	cmp cl, 0
+	jz _ret
 
-_get_len:
-	cmp byte[rdi], 0
-	je _end
-	inc rdi
-	inc rax
-	jmp _get_len
+_loop:
+	inc	rax
+	mov cl, [rax]
+	cmp cl, 0
+	jnz _loop
 
-_end:
+_ret:
+	sub rax, rdi
 	ret
